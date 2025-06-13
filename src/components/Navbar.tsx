8628b38307f0dd4e40570f013fd9b20ccdd71941
HriGrit <ayushsoam15@gmail.com>
2025-06-14 03:31:42 +0530

    fix: Navbar logo url fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import Logo from '../assets/logo.svg';
+import Logo from '../assets/Logo.svg';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
